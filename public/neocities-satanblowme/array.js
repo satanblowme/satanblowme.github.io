fa55e7e4046e977642329ee18ddf249a0017f45f
waterfeels <azimitravich@gmail.com>
2024-11-25 13:56:05 -0500

    uploading remote repository to main not sure if its in correct folder

---
+++ public/neocities-satanblowme/array.js
@@ -0,0 +1,12 @@
+
+   
+var images = ["img/apent55.gif","img/ASU.gif", "img/3dbonehead.gif","img/4RNn.gif","img/86yA.gif","img/Ryokosword.gif","img/TL80.gif"]
+
+var imgState = 0;
+
+var imgTag = document.getElementById("imgClick");
+
+imgTag.addEventListener("click", function (event)  {                    
+  imgState = (++imgState % 7);
+  event.target.src = images[imgState];
+});
